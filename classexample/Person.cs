ecfb2bb45cb16db5e870531a108580ebc6691b25
GuadalupeGFigueroa <guadalupe.g.figueroa@gmail.com>
2025-04-21 19:23:20 +0200

    File to practice creating private clssses and attributes completed. It works correctly.

---
+++ classexample/Person.cs
@@ -0,0 +1,39 @@
+namespace classexample;
+
+class Person
+{
+    private string name;
+    private string town;
+    //private int age; 
+    // No se define la variable "age" porque se establece una propiedad que creará la variable directamente. 
+
+    //Existen 3 formas de acceder a tributos privados.
+    //Forma 1: crear dos métodos para acceder al valor, uno para fijarlo y otro para obtenerlo.
+
+    public void setName(string name)
+    {
+        this.name = name; 
+    }
+    
+    public string getName()
+    {
+        return name;
+    }
+
+    //Forma 2: crear una propiedad con el mismo nombre del campo (con la inicial en mayúscula).
+    public string Town
+    {
+        get 
+        { 
+            return town;
+        }
+        set
+        {
+            town=value;
+        }
+    }
+     // Forma 3: crear propiedad. 
+        //Evita definir la variable
+    public int Age { get; set; }
+
+}
---
+++ classexample/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace classexample;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Clear();
+        Person p= new Person();
+        p.setName("Lupe");
+        Console.WriteLine(p.getName());
+        p.Town="Oviedo";
+        Console.WriteLine(p.Town);
+        p.Age=39;
+        Console.WriteLine(p.Age);
+    }
+}
